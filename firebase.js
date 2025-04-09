09a966cd83c300c015c5213efaa79808a6eaa33b
HHJ74 <gpgus0704@gmail.com>
2025-04-09 16:51:36 +0900

    refactor : firebase native 설정 -> Managed Workflow 설정 변경

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.firebaseApiKey,
+  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+  appId: Constants.expoConfig.extra.firebaseAppId,
+};
+
+const app = initializeApp(firebaseConfig);
+const firebaseDB = getFirestore(app);
+
+export { firebaseDB };
